acd2ac5efa0c43c4a543c967e3b981d42bcd2da3
alexisvisco <alexis.viscogliosi@outlook.fr>
2020-03-31 17:12:30 +0200

    spec: change regions completion by adding aws/ as a prefix

--- src/main/java/com/qovery/intellij/plugin/completion/domain/ApplicationSpec.java
+++ src/main/java/com/qovery/intellij/plugin/completion/domain/ApplicationSpec.java
@@ -8,29 +8,30 @@ public class ApplicationSpec {
     @Spec.ValueProject
     public String project;
 
-    @Spec.ValueList({"us-east-2",
-            "us-east-1",
-            "us-west-1",
-            "us-west-2",
-            "ap-east-1",
-            "ap-south-1",
-            "ap-northeast-3",
-            "ap-northeast-2",
-            "ap-southeast-1",
-            "ap-southeast-2",
-            "ap-northeast-1",
-            "ca-central-1",
-            "cn-north-1",
-            "cn-northwest-1",
-            "eu-central-1",
-            "eu-west-1",
-            "eu-west-2",
-            "eu-west-3",
-            "eu-north-1",
-            "me-south-1",
-            "sa-east-1",
-            "us-gov-east-1",
-            "us-gov-west-1"})
+    @Spec.ValueList({
+            "aws/us-east-2",
+            "aws/us-east-1",
+            "aws/us-west-1",
+            "aws/us-west-2",
+            "aws/ap-east-1",
+            "aws/ap-south-1",
+            "aws/ap-northeast-3",
+            "aws/ap-northeast-2",
+            "aws/ap-southeast-1",
+            "aws/ap-southeast-2",
+            "aws/ap-northeast-1",
+            "aws/ca-central-1",
+            "aws/cn-north-1",
+            "aws/cn-northwest-1",
+            "aws/eu-central-1",
+            "aws/eu-west-1",
+            "aws/eu-west-2",
+            "aws/eu-west-3",
+            "aws/eu-north-1",
+            "aws/me-south-1",
+            "aws/sa-east-1",
+            "aws/us-gov-east-1",
+            "aws/us-gov-west-1"})
     @Spec.KeyName("cloud_region")
     public String cloudRegion;
 
